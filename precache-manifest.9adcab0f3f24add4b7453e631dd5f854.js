cd1822afd3d29dfd6ba89ac1bf77883a67bf0073
Gaurav <gaurav.sinha1563@gmail.com>
2019-07-03 15:35:58 -0500

    Learning React for Work.

---
+++ precache-manifest.9adcab0f3f24add4b7453e631dd5f854.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ecc85978828286274abf8f55652f2ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d76028dda28ea3dd7bbc",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "d76028dda28ea3dd7bbc",
+    "url": "/static/js/main.57f0efb5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
